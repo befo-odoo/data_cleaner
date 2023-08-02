a5b980b8c41697e0a20ba239bb1fdf66d178dadd
befo <befo@odoo.com>
2023-08-02 11:11:36 -0400

    [ADD] data_cleaner: scaffolding for module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Data Import Cleaning",
+    "website": "https://www.odoo.com",
+    "author": "Odoo Inc.",
+    "summary": "Interface for cleaning and formatting uploaded files",
+    "description": "",
+    "category": "Custom Development",
+    "depends": [],
+    "data": [
+        "views/data_cleaner_views.xml"
+    ],
+    "website": "www.odoo.com",
+    "application": True,
+}

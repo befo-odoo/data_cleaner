745c2f1dbfbe4c1456180d69ff637223663213ca
befo <befo@odoo.com>
2023-08-03 14:39:42 -0400

    [REL] data_cleaner: add model modifiers, change layout

--- models/data_cleaner.py
+++ models/data_cleaner.py
@@ -4,18 +4,19 @@ class DataCleaner(models.Model):
     _name = 'data.cleaner'
     _description = 'Tool for importing and cleaning client data'
     
-    loaded = fields.Boolean(string="File Loaded", default=False)
+    file_loaded = fields.Boolean(string="File Loaded", default=False)
     file = fields.Binary(string="Uploaded File")
 
-    # Open a file and set its loaded status
+    # Import a file, set its loaded status
     def import_csv(self):
         for record in self:
             record.file = None #TODO [upload and set file]
-            record.loaded = True
+            record.file_loaded = True
+
 
     # Export the formatted file and set variables back to default
     def export_csv(self):
         for record in self:
             #TODO [download file here]
             record.file = None
-            record.loaded = False
+            record.file_loaded = False

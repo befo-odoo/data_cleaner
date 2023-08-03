66dbf6bbdba8a08d2b9e3f91663c9a8521378e0c
Daniel Nathanson <dana@odoo.com>
2023-08-03 14:52:43 -0400

    [REL] data_cleaner: Implemented the download cleaned csv button

--- models/data_cleaner.py
+++ models/data_cleaner.py
@@ -1,11 +1,13 @@
 from odoo import api, fields, models
-
+import base64
 class DataCleaner(models.Model):
     _name = 'data.cleaner'
     _description = 'Tool for importing and cleaning client data'
     
-    loaded = fields.Boolean(string="File Loaded", default=False)
+    file_loaded = fields.Boolean(string="File Loaded", default=True)
     file = fields.Binary(string="Uploaded File")
+    cleaned_csv = fields.Char(default='Test')
+    exportable_csv = fields.Binary()
 
     # Open a file and set its loaded status
     def import_csv(self):
@@ -19,3 +21,13 @@ class DataCleaner(models.Model):
             #TODO [download file here]
             record.file = None
             record.loaded = False
+
+
+    def download_cleaned_csv(self):
+        self.exportable_csv = base64.b64encode(bytes(self.cleaned_csv, encoding='utf8'))
+        return {
+            'name': 'FEC',
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/?model=data.cleaner&id={self.id}&field=exportable_csv&filename=cleaned_csv.csv&download=true',
+            'target': 'self',
+        }

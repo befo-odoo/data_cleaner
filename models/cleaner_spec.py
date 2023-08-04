991bad53b6003b54a4a45cb74f0e9fc6d780920b
befo <befo@odoo.com>
2023-08-04 13:51:59 -0400

    [IMP] data_cleaner: add empty methods and create models

--- models/cleaner_spec.py
+++ models/cleaner_spec.py
@@ -1,5 +1,58 @@
 from odoo import models, api, fields
 class CleanerSpec(models.TransientModel):
     _name = 'cleaner.spec'
+    _description = 'data cleaner specificiation'
 
+    # Process data into correct structure for exporting
+    def process_data(self, data):
+        # Group attributes by product:
+        #
+        # [
+        #   prod1: {
+        #       attr1: [val1, val2, val3],
+        #       attr2: [val4, val5]
+        #   },
+        #   prod2: {
+        #       attr1: [val1, val6, val7],
+        #       attr2: [val5, val8]
+        #   }
+        # ]
+        #
+        return
+
+class CleanerSpecColumn(models.Model):
+    _name = 'cleaner.spec.column'
+    _description = 'data cleaner specificiation column'
+
+    column_type = fields.Selection([
+        ('attribute', 'Attribute'),
+        ('none', 'Not an attribute')],
+        string='Request Type')
     
+    attrs = fields.One2many(
+        string='Attributes', 
+        comodel_name='cleaner.spec.val',
+        inverse_name='val'
+    )
+    vals = fields.Many2many(
+        string='Values', 
+        comodel_name='cleaner.spec.attr', 
+        relation='attr_val_pairs', 
+        column1='value', 
+        column2='attribute'
+    )
+
+    # Receive a column and process it
+    def process_column(self, col_data):
+        if self.column_type is 'attribute':
+            return
+
+class CleanerSpecAttr(models.Model):
+    _name = 'cleaner.spec.attr'
+    _description = 'cleaner specification attribute'
+    attr = fields.Char(string='Attribute')
+
+class CleanerSpecVal(models.Model):
+    _name = 'cleaner.spec.val'
+    _description = 'cleaner specification value'
+    val = fields.Char(string='Value')

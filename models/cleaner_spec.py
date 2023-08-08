45bb8d11dd6320175d3e46fa60a25b940f576bf8
befo <befo@odoo.com>
2023-08-08 15:03:01 -0400

    [IMP] data_cleaner: continue parsing dirty csv into python dict"

--- models/cleaner_spec.py
+++ models/cleaner_spec.py
@@ -1,16 +1,20 @@
 from odoo import models, api, fields
 from xml.etree import ElementTree as etree
 from csv import DictReader
-from io import StringIO
-import json
+
 class CleanerSpec(models.TransientModel):
     _name = 'cleaner.spec'
     _description = 'data cleaner specificiation wizard'
 
-    product_header = fields.Char(string="CSV Header for Product")
+    # product_header = fields.Selection(selection=[('','')], string="CSV Header for Product")
+    product_header = fields.Char(string="Product ID")
     cols = fields.Char(string='Columns', default='')
     attrs = fields.Char(string='Attributes', default='')
     vals = fields.Char(string='Values', default='')
+
+    parent = fields.Many2one(comodel_name='data.cleaner', string='Parent Object')
+
+
 
     # Process dirty data into correct structure for exporting
     # Group attributes by product:
@@ -24,12 +28,13 @@ class CleanerSpec(models.TransientModel):
     #       attr2: [val5, val8]
     #   },
     # ]
-    def process_data(self, data):
+    def process_data(self, buf):
+        self.cols = self.attrs = self.vals = ''
+        data = DictReader(buf)
         self.process_headers(data)
 
     def process_headers(self, data):
         # Add variable number of column names to wizard
-        # self.ensure_one()
         fields_view = self.env.ref('data_cleaner.view_cleaner_spec_form')
         arch = etree.fromstring(fields_view.arch)
 
@@ -40,19 +45,26 @@ class CleanerSpec(models.TransientModel):
             # Trigger if column is attribute
             if True: self.attrs += header + ','
             # Add header to list of column names
-            self.cols += header
+            self.cols += header + ','
 
-        # Strip trailing comma
+        # Set domain of the product header field to all available columns
+        # self.product_header = [(col, col) for col in self.cols]
+
+        # Strip trailing commas
         self.attrs = self.attrs[:-1]
+        self.cols = self.cols[:-1]
         
-        for index, field_value in enumerate(self.cols, start=1):
-            field_name = f'dynamic_field_{index}'
-            field = etree.Element('field', {'name': field_name})
+        # Build variable number of fields
+        for index, field_name in enumerate(self.cols.split(','), start=1):
+            label = etree.Element('h6')
+            label.text = field_name
+            arch.append(label)
+            field = etree.Element('input', {'type': 'checkbox', 'id': f'attr{index}'})
             arch.append(field)
 
+        # Assign variable number of fields architecture to the view
         fields_view.arch = etree.tostring(arch)
-        return
     
     # Generate clean csv file for importing
-    def generate_csv(self):
-        return "test,test1"
+    # def generate_csv(self):
+    #     return "test,test1"

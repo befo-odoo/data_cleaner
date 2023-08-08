7005da92232443746a141d5cd32b02dfa892fbf0
Aviral Petwal <avpe@pop-os.localdomain>
2023-08-08 11:35:59 -0400

    [IMP] Added the logic to create a clean downloadable csv file from the raw data

--- models/cleaner_spec.py
+++ models/cleaner_spec.py
@@ -3,6 +3,10 @@ from xml.etree import ElementTree as etree
 from csv import DictReader
 from io import StringIO
 import json
+import pandas as pd
+import io
+
+
 class CleanerSpec(models.TransientModel):
     _name = 'cleaner.spec'
     _description = 'data cleaner specificiation wizard'
@@ -44,7 +48,7 @@ class CleanerSpec(models.TransientModel):
 
         # Strip trailing comma
         self.attrs = self.attrs[:-1]
-        
+
         for index, field_value in enumerate(self.cols, start=1):
             field_name = f'dynamic_field_{index}'
             field = etree.Element('field', {'name': field_name})
@@ -52,7 +56,33 @@ class CleanerSpec(models.TransientModel):
 
         fields_view.arch = etree.tostring(arch)
         return
-    
+
     # Generate clean csv file for importing
-    def generate_csv(self):
-        return "test,test1"
+    def generate_csv(self, data):
+        print("Cleaning data")
+        df = pd.read_csv(data, sep="\t")
+        attribute_array = ["Manufacturer",
+                           "Collection",
+                           "Color",
+                           "Vendor_SKU",
+                           "Designer",
+                           "Fabric_Type",
+                           "Fiber_Contents",
+                           "Fabric_Width",
+                           "Putup_Format"]
+        new_col_list = [col_name for col_name in df.columns.values if col_name not in attribute_array]
+        updated_col_list = new_col_list.copy()
+        updated_col_list.extend(["Attribute", "Values"])
+        updated_df = pd.DataFrame(columns=updated_col_list)
+        j = 0
+        for index, row in df.iterrows():
+            print("Processing Row " + str(j))
+            j = j + 1
+            first_row = {col: row[col] for col in new_col_list}
+            first_row['Attribute'] = attribute_array[0]
+            first_row['Values'] = row[attribute_array[0]]
+            updated_df = pd.concat([updated_df, pd.DataFrame([first_row])], ignore_index=True)
+            for i in range(1, len(attribute_array)):
+                temp_dict = {'Attribute': attribute_array[i], 'Values': row[attribute_array[i]]}
+                updated_df = pd.concat([updated_df, pd.DataFrame([temp_dict])], ignore_index=True)
+        return updated_df.to_csv(index=False)
